80eeac6891d06c11aeaa567b6114c3ca39b87ab5
KDeepu3227 <deepugmd0317@gmail.com>
2024-12-23 20:38:16 +0530

    Create usingseries.py

---
+++ usingseries.py
@@ -0,0 +1,22 @@
+###
+write a python program using series?
+###
+data = [10,20,30,40]
+series = pd.Series(data,index=['A','B','C','D'])
+print(series)
+data = ['deepu','manasa','sumi','chandu','pravalik']
+series = pd.Series(data,index=[1,2,3,4,5])
+print(series)
+###
+A    10
+B    20
+C    30
+D    40
+dtype: int64
+1       deepu
+2       madhu
+3        sumi
+4      chandu
+5    pravalik
+dtype: object
+  
